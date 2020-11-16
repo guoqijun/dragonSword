5ee8176158528e88650da944be9e08fd6841bae5
guoqijun <guoqijun1994@gmail.com>
2020-11-16 08:48:52 +0800

    add solution

--- src/main/java/com/quinn/code/medium/LongestPalindromeSolution.java
+++ src/main/java/com/quinn/code/medium/LongestPalindromeSolution.java
@@ -10,6 +10,47 @@ package com.quinn.code.medium;
 //注意: "aba" 也是一个有效答案。
 public class LongestPalindromeSolution {
     public String longestPalindrome(String s) {
-        return null;
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        if (s.length() == 1) {
+            return s;
+        }
+        String maxStr = s.substring(0, 1);
+        boolean result;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = s.length() - 1; j > i; j--) {
+                result = isMaxStr(s, i, j);
+                if (result) {
+                    if (s.substring(i, j+1).length() > maxStr.length()) {
+                        maxStr = s.substring(i, j + 1);
+                    }
+                    break;
+                }
+            }
+        }
+
+        return maxStr;
+    }
+
+    public boolean isMaxStr(String s, int start, int end) {
+        while (true) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            if ((end - start) > 2) {
+                end--;
+                start++;
+            } else {
+                return true;
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        LongestPalindromeSolution longestPalindromeSolution = new LongestPalindromeSolution();
+        String result = longestPalindromeSolution.longestPalindrome("cbbd");
+        System.out.println(result);
     }
 }
