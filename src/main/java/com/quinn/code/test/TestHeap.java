3cb6d6562a1f02061f1abcfa30117bc368eadb7e
guoqijun <guoqijun1994@gmail.com>
2021-03-28 10:45:28 +0800

    add solution

---
+++ src/main/java/com/quinn/code/test/TestHeap.java
@@ -0,0 +1,28 @@
+package com.quinn.code.test;
+
+public class TestHeap {
+    public static void main(String[] args) {
+//  SpringApplication.run(DemoApplication.class, args);
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            System.out.println("times:" + i + " , result:" + testExceptionTrunc());
+        }
+    }
+
+    public static boolean testExceptionTrunc()  {
+        try {
+            // 人工构造异常抛出的场景
+            ((Object)null).getClass();
+        } catch (Exception e) {
+            if (e.getStackTrace().length == 0) {
+                try {
+                    // 堆栈消失的时候当前线程休眠5秒，便于观察
+                    Thread.sleep(5000);
+                } catch (InterruptedException interruptedException) {
+                    // do nothing
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
