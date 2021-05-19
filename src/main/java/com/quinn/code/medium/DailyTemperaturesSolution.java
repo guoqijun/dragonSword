ce0eba024392b52f7852b4045f7959eee734ad5f
guoqijun <guoqijun1994@gmail.com>
2021-05-19 23:22:35 +0800

    add solution

--- src/main/java/com/quinn/code/medium/DailyTemperaturesSolution.java
+++ src/main/java/com/quinn/code/medium/DailyTemperaturesSolution.java
@@ -1,6 +1,23 @@
 package com.quinn.code.medium;
 
+import java.util.Stack;
+
 public class DailyTemperaturesSolution {
+
+    public int[] dailyTemperatures2(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; ++i) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int temp = stack.pop();
+                res[temp] = i - temp;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+
+
     public int[] dailyTemperatures(int[] temperatures) {
         int len = temperatures.length;
         if (len == 1) {
---
+++ src/main/java/com/quinn/code/medium/LengthOfLISSolution.java
@@ -0,0 +1,52 @@
+package com.quinn.code.medium;
+
+public class LengthOfLISSolution {
+    //[10,9,2,5,3,7,101,18]
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return 0;
+        }
+        if (len == 1) {
+            return 1;
+        }
+        int[] dp = new int[len];
+        dp[0] = 1;
+
+        int indexDp = 1;
+        int result = 1;
+        for (int index = 1; index < len; index++) {
+            indexDp = 1;
+            for (int i = 0; i < index; i++) {
+                if (nums[index] > nums[i]) {
+                    indexDp = Math.max(indexDp, dp[i] + 1);
+                }
+            }
+
+            dp[index] = indexDp;
+            result = Math.max(result, dp[index]);
+        }
+
+        return result;
+    }
+
+    //[0,1,0,3,2,3]
+    public int lengthOfLIS2(int[] nums) {
+        int result = 1;
+        int len = nums.length;
+
+        for (int index = 0; index < len; index++) {
+            int dist = nums[index];
+            int tempResult = 1;
+            for (int move = index; move < len; move++) {
+                if (nums[move] > dist) {
+                    tempResult++;
+                    dist = nums[move];
+                }
+            }
+            result = Math.max(result, tempResult);
+        }
+
+        return result;
+    }
+}
