0cdbe4d993485fd52e0ef17fdec9b65d7933491b
guoqijun <guoqijun1994@gmail.com>
2022-07-13 22:19:48 +0800

    add

--- GO/src/main.go
+++ GO/src/main.go
@@ -3,3 +3,25 @@ package main
 //程序入口
 func main() {
 }
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	ans := make([]int, 0)
+	loop(ans, root)
+	return ans
+}
+
+func loop(ans []int, root *TreeNode) []int {
+	if root != nil {
+		loop(ans, root.Left)
+		ans = append(ans, root.Val)
+		loop(ans, root.Right)
+	}
+
+	return ans
+}
